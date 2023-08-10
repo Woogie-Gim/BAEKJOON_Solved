0c891f417286d822533a41474c3488d84a663b07
김선욱 <tjsdnr960@naver.com>
2023-08-10 20:55:09 +0900

    1436 while문 사용

---
+++ StudyGroup/20230810/#1436.py
@@ -0,0 +1,11 @@
+n = int(input())
+movie_name = 666
+count = 0
+
+while True:
+    if '666' in str(movie_name):
+        count += 1
+    if count == n:
+        print(movie_name)
+        break
+    movie_name += 1
