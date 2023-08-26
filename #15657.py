0e8a87cf6d5bd18f0a2f567446e8dfb246cbe5c1
김선욱 <tjsdnr960@naver.com>
2023-08-26 20:42:30 +0900

    N과 M 7

---
+++ #15657.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+path = [0] * N
+def abc(start, level):
+    if level == M:
+        for i in range(M):
+            print(path[i], end = ' ')
+        print()
+        return
+
+    for i in range(start, N):
+        path[level] = arr[i]
+        abc(i, level + 1)
+        
+        
+abc(0, 0)
