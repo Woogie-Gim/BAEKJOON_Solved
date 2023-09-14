9987570cd09cdc243001da40a3e00d85df7ef416
Seon Wook Gim <tjsdnr960@naver.com>
2023-09-14 14:09:28 +0900

    #3003 입력받고 계산값 출력하기

---
+++ C++/#3003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3, num4, num5, num6;
+	cin >> num1;
+	cin >> num2;
+	cin >> num3;
+	cin >> num4;
+	cin >> num5;
+	cin >> num6;
+
+	cout << 1 - num1 << " " << 1 - num2 << " " << 2 - num3 << " " << 2 - num4 << " " << 2 - num5 << " " << 8 - num6 << "\n";
+	return 0;
+}
