221050ec8e487540b2454ce2efb66e45de9953ab
Seon Wook Gim <tjsdnr960@naver.com>
2023-09-22 16:55:27 +0900

    #1919 DAT 활용하여 비교하기

---
+++ C++/#1919.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int alphabet1[26], alphabet2[26];
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+
+	for (int i = 0; i < a.length(); i++)
+	{
+		alphabet1[a[i] - 'a']++;
+	}
+		
+
+	for (int i = 0; i < b.length(); i++)
+	{
+		alphabet2[b[i] - 'a']++;
+	}
+		
+
+	int cnt = 0;
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (alphabet1[i] != alphabet2[i])
+		{
+			cnt += abs(alphabet1[i] - alphabet2[i]);
+		}
+
+	}
+
+	cout << cnt << "\n";
+
+	return 0;
+}
