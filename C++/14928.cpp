e59949a8e5fe7b1066e3bf2de1cba1d0d75f5b22
김선욱 <tjsdnr960@naver.com>
2023-10-29 18:21:51 +0900

    #14928 큰 수

---
+++ C++/14928.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MOD = 20000303;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	string s;
+	cin >> s;
+
+	int result = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		int digit = s[i] - '0';
+		result = ((result * 10) % MOD + digit) % MOD;
+	}
+
+	cout << result << "\n";
+
+	return 0;
+}
