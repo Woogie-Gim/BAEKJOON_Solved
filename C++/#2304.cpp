d40bc8ded98adb52b8ef587679e9f0b2c9b91c97
김선욱 <tjsdnr960@naver.com>
2024-01-06 18:12:47 +0900

    #2304 창고 다각형

---
+++ C++/#2304.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+int d[1001][3];
+int answer, maxVal;
+int n, a, b;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> a >> b;
+        d[a][0] = b;
+    }
+
+    for (int i = 0; i <= 1000; i++) 
+    {
+        maxVal = max(d[i][0], maxVal);
+        d[i][1] = maxVal;
+    }
+
+    maxVal = 0;
+
+    for (int i = 1000; i >= 0; i--) 
+    {
+        maxVal = max(d[i][0], maxVal);
+        d[i][2] = maxVal;
+    }
+
+    for (int i = 0; i <= 1000; i++)
+    {
+        answer += min(d[i][1], d[i][2]);
+    }
+
+    cout << answer << "\n";
+
+    return 0;
+}
