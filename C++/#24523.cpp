ed11a8e614aaaece130fbccd1de7821d7d813cbc
김선욱 <tjsdnr960@naver.com>
2024-02-09 13:11:52 +0900

    #24523 내 뒤에 나와 다른 수

---
+++ C++/#24523.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, num;
+    vector<pair<int, int>> v;
+    stack<pair<int, int>> s;
+    vector<int> res;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        v.push_back({i + 1, num});
+    }
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        if(s.empty()) res.push_back(-1);
+        else
+        {
+            if(v[i].second == s.top().second) s.pop();
+            if(!s.empty()) res.push_back(s.top().first);
+            else res.push_back(-1);
+        }
+        s.push({v[i].first, v[i].second});
+    }
+    for (int i = res.size() - 1; i >= 0; i--)
+    {
+        cout << res[i] << " ";
+    }
+    
+    return 0;
+}
