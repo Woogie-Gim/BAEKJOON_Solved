e6b244dba59f4c3dc340cb68216a025583bef1da
김선욱 <tjsdnr960@naver.com>
2024-03-18 21:15:25 +0900

    #28278 스택 2

---
+++ C++/#28278.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, num;
+    
+	stack<int> stack;
+    
+	cin >> n;
+    
+	for (int i = 0; i < n; i++) 
+    {
+		cin >> m;
+		switch (m) 
+        {
+		case 1:
+			cin >> num;
+			stack.push(num);
+			break;
+		case 2:
+			if (!stack.empty()) 
+            {
+				cout << stack.top() << "\n";
+				stack.pop();
+			}
+			else cout << "-1\n";
+			break;
+		case 3:
+			cout << stack.size() << "\n";
+			break;
+		case 4:
+			if (stack.empty()) cout << "1\n";
+			else cout << "0\n";
+			break;
+		case 5:
+			if (!stack.empty()) cout << stack.top() << "\n";
+			else cout << "-1\n";
+			break;
+		}
+	}
+	return 0;
+}
