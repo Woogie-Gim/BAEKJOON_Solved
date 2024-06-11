dee8f9d4cbc8c822cc218211bde215131d3b9e41
SeonWook Gim <tjsdnr960@naver.com>
2024-06-11 14:52:16 +0900

    #18115 카드 놓기

---
+++ C++/#18115.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+    
+	int n, action;
+    
+	cin >> n;
+    
+	vector<int> res(n, 0);
+    
+	int top=0, middle= n == 1 ? 0 : 1, bottom=n-1;
+    
+	for (int i=n; i>0; i--) 
+    {
+		cin >> action;
+		if (action == 1) 
+        {
+			res[top] = i;
+			top = middle++;
+		} 
+        else if (action == 2) 
+        {
+			res[middle++] = i;
+		} 
+        else 
+        {
+			res[bottom--] = i;
+		}
+		if (middle > bottom) 
+        {
+			middle = top;
+			bottom = top;
+		}
+	}
+	for (auto r : res) 
+    {
+		cout << r << " ";
+	}
+}
