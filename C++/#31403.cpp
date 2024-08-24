98c29916b884e7c1e6070c7faebea486230f3ace
SeonWook Gim <tjsdnr960@naver.com>
2024-08-24 20:24:49 +0900

    #31403 A + B - C

---
+++ C++/#31403.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string A, B, C;
+
+    cin >> A >> B >> C;
+
+    cout << stoi(A) + stoi(B) - stoi(C) << '\n';
+    cout << stoi(A + B) - stoi(C) << '\n';
+
+    return 0;
+}
