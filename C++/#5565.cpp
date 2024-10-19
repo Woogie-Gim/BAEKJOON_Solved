cf1068d2f5a45bd9e1ae779996e57e242b8ca503
SeonWook Gim <tjsdnr960@naver.com>
2024-10-19 17:53:35 +0900

    #5565 영수증

---
+++ C++/#5565.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int totalPrice;
+	int sum = 0;
+	int num;
+
+	cin >> totalPrice;
+
+	for (int i = 0; i < 9; ++i) 
+    {
+		cin >> num;
+		sum += num;
+	}
+
+	cout << totalPrice - sum << "\n";
+
+	return 0;
+}
