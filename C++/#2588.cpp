f7d1c49f7aac282e7dbb3100d1cb3e04f7cc592c
SeonWook Gim <tjsdnr960@naver.com>
2024-11-04 16:27:19 +0900

    #2588 곱셈

---
+++ C++/#2588.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int A,B;
+	cin >> A ;
+	cin >> B ;
+	
+	cout << A*(B%10) << "\n";
+	cout << A*((B%100 - B%10)/10) << "\n";
+	cout << A*((B- (B % 100 - B % 10))/100) << "\n";
+	cout << A*B  << "\n";
+
+}
