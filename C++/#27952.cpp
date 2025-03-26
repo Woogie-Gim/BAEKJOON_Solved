3ea7542cfe9cb8d18b316e3911a5a9c06a98080c
SeonWook Gim <tjsdnr960@naver.com>
2025-03-26 20:14:52 +0900

    #27952 보디빌딩

---
+++ C++/#27952.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+long long int arr1[500001];
+long long int arr2[500001];
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long int n, x;
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr1[i];
+    }
+
+    long long int sum = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr2[i];
+        sum += arr2[i];
+
+        if (sum < arr1[i]) 
+        {
+            cout << -1;
+            return 0;
+        }
+    }
+
+    cout << (sum - arr1[n - 1]) / x;
+    
+    return 0;
+}
