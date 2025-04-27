d6081603ff61fea082959b37ae9df2d060bb4344
SeonWook Gim <tjsdnr960@naver.com>
2025-04-27 14:03:43 +0900

    #22858 원상 복구(small)

---
+++ C++/#22858.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory.h>
+ 
+using namespace std;
+ 
+int D[10001], S[10001], v[10001];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, K; cin>>N>>K;
+    
+    for(int i = 1; i <= N; i++)
+    {
+        cin>>S[i];
+    }
+    for(int i = 1; i <= N; i++)
+    {
+        cin>>D[i];
+    }
+    
+    while(K--)
+    {
+        for(int i = 1; i <= N; i++)
+        {
+            v[D[i]] = S[i];
+        }
+        memcpy(S,v,sizeof(S));
+    }
+    
+    for(int i = 1; i <= N; i++)
+    {
+        cout<< v[i] << ' ';
+    } 
+    
+    return 0;
+}
