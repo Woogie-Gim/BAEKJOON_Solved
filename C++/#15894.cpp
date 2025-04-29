7664d8017f5fd242509ec92b255a9a5d95998000
SeonWook Gim <tjsdnr960@naver.com>
2025-04-29 16:51:26 +0900

    #15894 수학은 체육과목 입니다

---
+++ C++/#15894.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    unsigned int N;
+    
+    cin >> N;
+    
+    cout << 4 * N;
+    
+    return 0;
+}
