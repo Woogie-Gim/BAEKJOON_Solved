b120252af0eacaad84d73845ae58d1a9fa4521b5
Seon Wook Gim <tjsdnr960@naver.com>
2025-06-16 10:14:00 +0900

    #27969 I LOVE JavaScript

---
+++ C++/#27969.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+#include <stack>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    stack<long long> s;
+
+    string str;
+
+    while (cin >> str) 
+    {
+        if (str == "[")
+        {
+            s.push(0);
+        } 
+        else if (isdigit(str[0]))
+        {
+            s.push(8);
+        } 
+        else if (str == "]") 
+        {
+            long long tmp = 0;
+            
+            while (s.top() != 0) 
+            {
+                tmp += s.top();
+                s.pop();
+            }
+            
+            s.pop();
+            s.push(tmp + 8);
+            
+        } 
+        else 
+        {
+            s.push(str.size() + 12);
+        }
+    }
+    cout << s.top();
+    
+    return 0;
+}
