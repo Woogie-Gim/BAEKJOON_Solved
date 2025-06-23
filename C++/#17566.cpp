fd253ef6ce94a8de1915b0cf7b59e4f420b7bcd2
Seon Wook Gim <tjsdnr960@naver.com>
2025-06-24 08:40:58 +0900

    #17566 Bus Logic

---
+++ C++/#17566.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve()
+{
+    int m, b, s; cin >> m >> b >> s;
+
+    vector<long long> v(b, 0);
+
+    for (int i = 0; i < b; i++) 
+    {
+        string t; 
+        cin >> t;
+        reverse(t.begin(), t.end());
+        
+        for (char c : t) v[i] = v[i] * 2 + (c == '1');
+    }
+
+    long long ans = 0;
+    
+    for (int i = 0; i < b; i++) 
+    {
+        if ((v[i] >> (m - 1)) & 1) ans |= v[i];
+    }
+    
+    cout << (ans ? __builtin_popcountll(ans) - 1 : 0);
+}
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+    return 0;
+}
