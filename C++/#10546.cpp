859c419be88ed48649f38279a0585f50a6975c23
Seon Wook Gim <tjsdnr960@naver.com>
2025-07-05 13:48:59 +0900

    #10546 배부른 마라토너

---
+++ C++/#10546.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N; 
+    cin >> N;
+
+    string name;
+
+    unordered_map<string,int> umap;
+
+    for(int i = 0; i < N; i++)
+    {
+        cin >> name;
+        umap[name]++;
+    }
+
+    for(int i = 0; i < N - 1; i++)
+    {
+        cin >> name;
+        umap[name]--;
+    }
+
+    for(auto it = umap.begin(); it != umap.end(); it++)
+    {
+        if(it -> second != 0) cout << it -> first << '\n';
+    }
+}
