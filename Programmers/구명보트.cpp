164e44ac2dbeb1681f84fe3efff5e04e19cdfca6
Seon Wook Gim <tjsdnr960@naver.com>
2026-06-18 20:36:32 +0900

    프로그래머스 그리디 '구명보트'

---
+++ Programmers/구명보트.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) 
+{
+    sort(people.begin(), people.end());
+    
+    int answer = 0;
+    int light = 0;
+    int heavy = people.size() - 1;
+    
+    while (light <= heavy) 
+    {
+        if (light == heavy) 
+        {
+            answer++;
+            break;
+        }
+        
+        if (people[light] + people[heavy] <= limit) 
+        {
+            light++;
+            heavy--;
+        }
+        else 
+        {
+            heavy--;
+        }
+        
+        answer++;
+    }
+    
+    return answer;
+}
