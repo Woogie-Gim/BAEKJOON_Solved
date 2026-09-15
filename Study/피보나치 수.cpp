e965b1d7c5b6e6cdd455668679fd3cddafd5faf6
Seon Wook Gim <tjsdnr960@naver.com>
2026-09-15 16:47:13 +0900

    C++ 재귀 : 프로그래머스 - 피보나치 수

---
+++ Study/피보나치 수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    vector<long long> dp(n + 1, 0);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
